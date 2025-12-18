e9620fc2a9a49486f0806c1498951bbdf93a3559
nurdinizuberi <nurdinizuberi67@gmail.com>
2025-12-18 21:23:54 +0300

    Database sucess

--- supabase-config.example.js
+++ supabase-config.example.js
@@ -8,3 +8,12 @@ window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXB
 export function validate() {
   if (!window.SUPABASE_URL || !window.SUPABASE_ANON_KEY) throw new Error('Missing Supabase config');
 }
+
+
+  // const SUPABASE_URL = window.SUPABASE_URL || 'https://sojqynzunkacykxqhkgy.supabase.co';
+  // const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvanF5bnp1bmthY3lreHFoa2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDY5ODksImV4cCI6MjA4MTYyMjk4OX0.6gyYxbsRxoQQ97xVf1ENCtz0Qt1AlLS0NUr5-BDibSk';
+  // const useSupabase = SUPABASE_URL !== 'https://sojqynzunkacykxqhkgy.supabase.co' && SUPABASE_ANON_KEY !== 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvanF5bnp1bmthY3lreHFoa2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDY5ODksImV4cCI6MjA4MTYyMjk4OX0.6gyYxbsRxoQQ97xVf1ENCtz0Qt1AlLS0NUr5-BDibSk';
+
+
+  // const SUPABASE_URL = 'https://sojqynzunkacykxqhkgy.supabase.co';
+  // const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvanF5bnp1bmthY3lreHFoa2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDY5ODksImV4cCI6MjA4MTYyMjk4OX0.6gyYxbsRxoQQ97xVf1ENCtz0Qt1AlLS0NUr5-BDibSk';
